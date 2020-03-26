b8fba048528d2d802868d8e395d4e5e797769cf6
bendamqui <bendamqui@icloud.com>
2020-03-26 18:31:15 -0400

    Implement and tests basic ArrayQl methods

--- src/ArrayQl.php
+++ src/ArrayQl.php
@@ -4,29 +4,72 @@ namespace Bendamqui\ArrayQl;
 
 class ArrayQl extends ArrayChain
 {
-    /**
-     * @param string ...$columns
-     * @return self
-     */
     public function exclude(string ...$columns): self
     {
         return $this->map($this->applyExclude($columns));
     }
 
-    /**
-     * @param int $limit
-     * @param int $offset
-     * @return $this
-     */
+    public function equal($key, $value): self
+    {
+        return $this->filter($this->applyEqual($key, $value));
+    }
+
+    public function first()
+    {
+        return array_values($this->limit()->toArray())[0];
+    }
+
+    public function in(string $column, array $values): self
+    {
+        return $this->filter($this->applyIn($column, $values));
+    }
+
     public function limit(int $limit = 1, int $offset = 0): self
     {
         return $this->slice($offset, $limit);
     }
 
+    public function select(string ...$columns): self
+    {
+        return $this->map($this->applySelect($columns));
+    }
+
+    public function groupBy(string $column): self
+    {
+        return $this->reduce($this->applyGroupBy($column), []);
+    }
+
+    public function notEqual(string $key, $value): self
+    {
+        return $this->filter($this->applyNotEqual($key, $value));
+    }
+
     /**
-     * @param array $columns
-     * @return \Closure
+     * @param string $key
+     * @param array|self $values
+     * @return self
      */
+    public function notIn(string $key, $values): self
+    {
+        $values = $values instanceof self ? $values->toArray() : $values;
+        return $this->filter($this->applyNotIn($key, $values));
+    }
+
+    public function replace(string $column, $value): self
+    {
+        return $this->map($this->applyReplace($column, $value));
+    }
+
+    public function exists(): bool
+    {
+        return $this->count() > 0;
+    }
+
+    public function when(bool $bool, \Closure $closure): self
+    {
+        return $bool ? $closure($this) : $this;
+    }
+
     private function applyExclude(array $columns): \Closure
     {
         return function ($item) use ($columns) {
@@ -37,19 +80,41 @@ class ArrayQl extends ArrayChain
         };
     }
 
-    /**
-     * @param string ...$columns
-     * @return $this
-     */
-    public function select(string ...$columns): self
+    private function applyEqual(string $key, $value): \Closure
     {
-        return $this->map($this->applySelect($columns));
+        return fn($item) => $item[$key] === $value;
     }
 
-    /**
-     * @param array $columns
-     * @return \Closure
-     */
+    private function applyGroupBy(string $column): \Closure
+    {
+        return function ($acc, $row) use ($column): array {
+            if (isset($row[$column])) {
+                $acc[$row[$column]][] = $row;
+            }
+            return $acc;
+        };
+    }
+
+    private function applyIn(string $column, array $values): \Closure
+    {
+        return fn($item) => isset($item[$column]) && in_array($item[$column],  $values);
+    }
+
+    private function applyNotEqual(string $key, $value): \Closure
+    {
+        return fn($item) => $item[$key] !== $value;
+    }
+
+    private function applyNotIn(string $key, array $values): \Closure
+    {
+        return fn($item) => empty($values) || !in_array($item[$key] ?? null, $values);
+    }
+
+    private function applyReplace(string $column, $value): \Closure
+    {
+        return fn($item) => array_replace($item, [$column => $value]);
+    }
+
     private function applySelect(array $columns): \Closure
     {
         return function (array $item) use ($columns): array {
@@ -58,34 +123,5 @@ class ArrayQl extends ArrayChain
                 return isset($select[$index]);
             }, ARRAY_FILTER_USE_BOTH );
         };
-    }
-
-    /**
-     * @param string $column
-     * @return self
-     */
-    public function groupBy(string $column): self
-    {
-        return $this->reduce($this->applyGroupBy($column), []);
-    }
-
-    /**
-     * @param string $column
-     * @return \Closure
-     */
-    public function applyGroupBy(string $column): \Closure
-    {
-        return function ($acc, $row) use ($column) {
-            $acc[$row[$column]][] = $row;
-            return $acc;
-        };
-    }
-
-    /**
-     * @return bool
-     */
-    public function exists(): bool
-    {
-        return $this->count() > 0;
     }
 }
--- tests/ArrayQlTest.php
+++ tests/ArrayQlTest.php
@@ -5,18 +5,38 @@ namespace Test;
 use Bendamqui\ArrayQl\ArrayQl;
 use PHPUnit\Framework\TestCase;
 
+use function PHPUnit\Framework\assertCount;
+use function PHPUnit\Framework\assertFalse;
 use function PHPUnit\Framework\assertInstanceOf;
 use function PHPUnit\Framework\assertEquals;
+use function PHPUnit\Framework\assertNotEquals;
+use function PHPUnit\Framework\assertEmpty;
+use function PHPUnit\Framework\assertTrue;
 
 class ArrayQlTest extends TestCase
 {
-    /* @var array */
-    private $single = [
+    private array $assoc = [
+        ['id' => 1, 'name' => 'Pierre', 'age' => 20],
+        ['id' => 2, 'name' => 'Paul', 'age' => 30],
+        ['id' => 3, 'name' => 'Jacques', 'age' => 40],
+        ['id' => 4, 'name' => 'Jacques', 'age' => 50],
+    ];
+
+    private array $single = [
         ['id' => 1, 'name' => 'Pierre', 'age' => 46]
     ];
 
-    /* @var array */
-    private $simple = [1, 2, 3, 4, 5];
+    private array $simple = [1, 2, 3, 4, 5];
+
+    public function testExists()
+    {
+        assertTrue(ArrayQl::make([1])->exists());
+    }
+
+    public function testDoesntExist()
+    {
+        assertFalse(ArrayQl::make([])->exists());
+    }
 
     public function testExclude()
     {
@@ -25,16 +45,108 @@ class ArrayQlTest extends TestCase
         assertEquals([['id' => 1]], $result->toArray());
     }
 
+    public function testEqual()
+    {
+        $arrayQl = ArrayQl::make($this->assoc);
+        $initCount = $arrayQl->count();
+        $result = $arrayQl->equal('name', 'Jacques');
+        assertInstanceOf(ArrayQl::class, $result);
+        assertNotEquals($initCount, $result->count());
+        foreach ($result as $row) {
+            assertEquals('Jacques', $row['name']);
+        }
+    }
+
+    public function testEqualWithInvalidColumn()
+    {
+        $result = ArrayQl::make($this->assoc)->equal('email', 1);
+        assertEmpty($result);
+    }
+
     public function testExcludeWithInvalidAndValidColumns()
     {
         $result = ArrayQl::make($this->single)->exclude('name', 'email');
         assertEquals([['id' => 1, 'age' => 46]], $result->toArray());
     }
 
+    public function testFirst()
+    {
+        assertEquals(1, ArrayQl::make($this->simple)->first());
+    }
+
+    public function testIn()
+    {
+        $filter = ['Paul', 'Jacques'];
+        $result = ArrayQl::make($this->assoc)->in('name', $filter);
+        assertInstanceOf(ArrayQl::class, $result);
+        assertNotEquals(count($this->assoc), $result->count());
+        foreach ($result as $row) {
+            assertTrue(in_array($row['name'], $filter));
+        }
+    }
+
+    public function testInWithInvalidColumn()
+    {
+        $result = ArrayQl::make($this->assoc)->in('email', [1]);
+        assertEmpty($result);
+    }
+
+    public function testNotIn()
+    {
+        $filter = ['Pierre', 'Paul'];
+        $result = ArrayQl::make($this->assoc)->notIn('name', $filter);
+        assertInstanceOf(ArrayQl::class, $result);
+        assertNotEquals(count($this->assoc), $result->count());
+        foreach ($result as $row) {
+            assertFalse(in_array($row['name'], $filter));
+        }
+    }
+
+    public function testNotEqual()
+    {
+        $result = ArrayQl::make($this->assoc)->notEqual('name', 'Jacques');
+        assertInstanceOf(ArrayQl::class, $result);
+        assertNotEquals(count($this->assoc), $result->count());
+        foreach ($result as $row) {
+            assertNotEquals('Jacques', $row['name']);
+        }
+    }
+
+    public function testNotEqualWithInvalidColumn()
+    {
+        $result = ArrayQl::make($this->assoc)->notEqual('email', 'Jacques');
+        assertInstanceOf(ArrayQl::class, $result);
+        assertEquals(count($this->assoc), $result->count());
+    }
+
+    public function testReplace()
+    {
+        $result = ArrayQl::make($this->assoc)->replace('age', 30);
+        assertInstanceOf(ArrayQl::class, $result);
+        foreach ($result as $row) {
+            self::assertEquals(30, $row['age']);
+        }
+    }
+
+    public function testGroupBy()
+    {
+        $result = ArrayQl::make($this->assoc)->groupBy('name');
+        assertInstanceOf(ArrayQl::class, $result);
+        foreach (array_count_values(array_column($this->assoc, 'name')) as $key => $count) {
+            assertCount($count, $result[$key]);
+        }
+    }
+
+    public function testGroupByWithInvalidColumn()
+    {
+        $result = ArrayQl::make($this->assoc)->groupBy('email');
+        assertInstanceOf(ArrayQl::class, $result);
+        assertEmpty($result);
+    }
+
     public function testLimit()
     {
-        $arrayQl = ArrayQl::make([1,2,3,4]);
-        $result = $arrayQl->limit(2);
+        $result = ArrayQl::make([1,2,3,4])->limit(2);
         assertInstanceOf(ArrayQl::class, $result);
         assertEquals([1,2], $result->toArray());
     }
@@ -66,5 +178,25 @@ class ArrayQlTest extends TestCase
         $result = ArrayQl::make($this->single)
             ->select('email');
         assertEquals([[]], $result->toArray());
+    }
+
+    public function testWhenTrue()
+    {
+        $result = ArrayQl::make($this->assoc)
+            ->when(
+                true,
+                fn(ArrayQl $arrayQl) => $arrayQl->limit(1)
+            );
+        assertCount(1, $result);
+    }
+
+    public function testWhenFalse()
+    {
+        $result = ArrayQl::make($this->assoc)
+            ->when(
+                false,
+                fn(ArrayQl $arrayQl) => $arrayQl->limit(1)
+            );
+        assertCount(count($this->assoc), $result);
     }
 }
